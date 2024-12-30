a7a04a7b1d0c3c2e70c3dbfda6077c190aa8ae31
AfonsoManata <afonso.manata.cruz@gmail.com>
2024-12-30 17:34:24 +0000

    authorization

--- final_project/index.js
+++ final_project/index.js
@@ -11,8 +11,24 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+ // Verify if there is an authorization in this session
+    if (req.session.authorization) {
+        let token = req.session.authorization['accessToken']; // Obtaining the token
+        
+        // Validating the token JWT
+        jwt.verify(token, "fingerprint_customer", (err, decoded) => {
+            if (err){
+                return res.status(403).json({message: "User not authenticated"}) // Invalid Token
+            }
+            req.user = decoded; // Data od the user
+            next(); // Going to the next midlware or endpoint
+        });
+    
+    } else{
+        return res.status(403).json({message: "User not logged in" }); // The user don't have authorization
+    }
 });
+
  
 const PORT =5000;
 
